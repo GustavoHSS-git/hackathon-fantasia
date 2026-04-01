e3196b32182e885277f957f6d7db5132f1afb7d2
Nalaura <bernabelala01@gmail.com>
2026-03-31 21:11:24 -0300

    a

--- funcao/excluirFantasia.php
+++ funcao/excluirFantasia.php
@@ -25,9 +25,7 @@ $id = $_GET['id'];
 
     <button type="submit" name="excluir">Excluir</button>
 
-<a href="../lista/fantasia.php">
-    <button type="btn-cancelar">Cancelar</button>
-</a>
+    <button class="voltar"  href="#" onclick="history.back()">Cancelar</button>
 </form>
 
 </body>
